acadac4118b418727a9f617ce509d96ef47dd401
Zacharias Levander <zacharias.levander@mafy.fi>
2021-02-24 20:51:37 +0200

    Render AppBar in Main
    This marks my answer for exercise 10.4.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import RepositoryList from './RepositoryList';
+import AppBar from './AppBar';
 
 const Main = () => {
   return (
-   <RepositoryList/>
+    <>
+      <AppBar />
+      <RepositoryList/>
+    </>
   );
 };
 
