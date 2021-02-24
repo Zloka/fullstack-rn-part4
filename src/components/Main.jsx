aef94dbdf3343902e7f8ff074f7c211ab9ac2c62
Zacharias Levander <zacharias.levander@mafy.fi>
2021-02-24 20:26:17 +0200

    Render RepositoryList in Main
    This marks my answer for exercise 10.3.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-
-import Text from './Text';
+import RepositoryList from './RepositoryList';
 
 const Main = () => {
   return (
-    <>
-      <Text>Simple text</Text>
-      <Text style={{ paddingBottom: 10 }}>Text with custom style</Text>
-      <Text fontWeight="bold" fontSize="subheading">
-        Bold subheading
-      </Text>
-      <Text color="textSecondary">Text with secondary color</Text>
-    </>
+   <RepositoryList/>
   );
 };
 
