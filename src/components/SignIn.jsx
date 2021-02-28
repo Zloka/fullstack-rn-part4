f9a253caae9915fd35be829090c5a4f61cb9985a
Zacharias Levander <zacharias.levander@mafy.fi>
2021-02-28 10:32:24 +0200

    Validate the form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,17 @@
 import { Formik } from 'formik';
 import React from 'react';
+import * as yup from 'yup';
+
 import SignInForm from './SignInForm';
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required('Username is required!'),
+  password: yup
+    .string()
+    .required('Password is required'),
+});
 
 const initialValues = {
   username: '',
@@ -13,7 +24,7 @@ const SignIn = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
       {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
     </Formik>
   );
