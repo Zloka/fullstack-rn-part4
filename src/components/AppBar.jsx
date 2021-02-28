78a35cc194df5fc461987fe18741981c859303a7
Zacharias Levander <zacharias.levander@mafy.fi>
2021-02-28 15:36:26 +0200

    Allow signing out in the AppBar
    This marks my answer for exercise 10.16.

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
+import { useApolloClient } from '@apollo/client';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarRoute from './AppBarRoute';
+import useAuthenticatedUser from '../hooks/useAuthenticatedUser';
+import AuthStorageContext from '../contexts/AuthStorageContext';
 
 const styles = StyleSheet.create({
   container: {
@@ -13,11 +16,21 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const { data } = useAuthenticatedUser();
+  const authorizedUser = data?.authorizedUser;
+  const authStorage = useContext(AuthStorageContext);
+  const apolloClient = useApolloClient();
+
+  const handleSignOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView horizontal>
         <AppBarRoute to="/" text="Repositories" />
-        <AppBarRoute to="/signin" text="Sign in" />
+        {authorizedUser ? <AppBarRoute onPress={handleSignOut} text="Sign out" /> : <AppBarRoute to="/signin" text="Sign in" />}
       </ScrollView>
     </View>
   );
