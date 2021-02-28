487e2fd11b5a9684124df81d7f70c5b32b1228a7
Zacharias Levander <zacharias.levander@mafy.fi>
2021-02-28 15:10:23 +0200

    Update useSignIn to set token and reset store

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,11 +1,23 @@
+import { useContext } from 'react';
+import { useApolloClient } from '@apollo/client';
 import { useMutation } from "@apollo/react-hooks";
 import { SIGN_IN } from '../graphql/mutations';
+import AuthStorageContext from '../contexts/AuthStorageContext';
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(SIGN_IN);
+  const authStorage = useContext(AuthStorageContext);
+  const apolloClient = useApolloClient();
 
   const signIn = async ({ username, password }) => {
-    return mutate({ variables: { username, password }});
+    const retVal = await mutate({ variables: { username, password }});
+    const { data } = retVal;
+    const { authorize } = data;
+    const { accessToken } = authorize;
+    await authStorage.setAccessToken(accessToken);
+    await apolloClient.resetStore();
+
+    return retVal;
   };
 
   return [signIn, result];
