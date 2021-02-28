4f97f95441ddb147b4e5ec42d89640778747c8db
Zacharias Levander <zacharias.levander@mafy.fi>
2021-02-28 10:12:44 +0200

    Add basic styles to the textinput

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textInput: { height: 48, borderColor: 'gray', borderWidth: 1, borderRadius: 4 },
+});
 
 const TextInput = ({ style, ...props }) => {
-  const textInputStyle = [style];
+  const textInputStyle = [styles.textInput, style];
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
