92de9e1e4f7a52ca529e08191ed3fd7f55ada90e
Zacharias Levander <zacharias.levander@mafy.fi>
2021-02-28 09:40:23 +0200

    Update RepositoryList and RepositoryListItem
    This marks my answer for exercise 10.5.

--- src/components/RepositoryItem.js
+++ src/components/RepositoryItem.js
@@ -1,16 +1,53 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import theme from '../theme';
+import RepoNumbers from './RepoNumbers';
+import Text from './Text';
 
-const RepositoryItem = ({ id, fullName, description, language, forksCount, stargazersCount, ratingAverage, reviewCount }) => {
+const styles = StyleSheet.create({
+  flexContainer: {
+    flexDirection: 'row',
+  },
+  image: {
+    height: 48, width: 48,
+    borderRadius: 2,
+  },
+  container: {
+    padding: theme.margins.default,
+  },
+  textContainer: {
+    marginLeft: theme.margins.default,
+    flex: 1,
+  },
+  bottomMargin: {
+    marginBottom: 4
+  },
+  languageContainer: {
+    backgroundColor: 'blue',
+    padding: 4,
+    borderRadius: 4,
+    alignSelf: 'flex-start'
+  },
+  languageText: {
+    color: 'white'
+  }
+});
+
+
+const RepositoryItem = ({ id, fullName, description, language, forksCount, stargazersCount, ratingAverage, reviewCount, ownerAvatarUrl }) => {
   return (
-    <View key={id}>
-      <Text>{fullName}</Text>
-      <Text>{description}</Text>
-      <Text>{language}</Text>
-      <Text>{stargazersCount}</Text>
-      <Text>{forksCount}</Text>
-      <Text>{reviewCount}</Text>
-      <Text>{ratingAverage}</Text>
+    <View style={styles.container} key={id}>
+      <View style={styles.flexContainer}>
+        <Image style={styles.image} source={{ uri: ownerAvatarUrl }}  />
+        <View style={styles.textContainer}>
+          <Text style={styles.bottomMargin} fontWeight="bold" fontSize="subheading">{fullName}</Text>
+          <Text style={styles.bottomMargin}>{description}</Text>
+          <View style={styles.languageContainer}>
+            <Text style={styles.languageText} fontWeight="bold">{language}</Text>
+          </View>
+        </View>
+      </View>
+      <RepoNumbers forksCount={forksCount} stargazersCount={stargazersCount} ratingAverage={ratingAverage} reviewCount={reviewCount} />
     </View>
   );
 };
