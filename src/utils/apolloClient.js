a389917f18ee546e1d711ff80ac25c0b5d2f60d4
Zacharias Levander <zacharias.levander@mafy.fi>
2021-05-15 10:37:58 +0300

    Update Apollo dependencies and add InMemoryCache

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,21 +1,52 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Constants from 'expo-constants';
+import { relayStylePagination } from '@apollo/client/utilities';
+
+const { apolloUri } = Constants.manifest.extra;
+
+const httpLink = createHttpLink({
+  uri: apolloUri,
+});
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        repositories: relayStylePagination(),
+      },
+    },
+    Repository: {
+      fields: {
+        reviews: relayStylePagination(),
+      },
+    },
+  },
+});
 
 const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      };
+    } catch (e) {
+      console.log(e);
+
+      return {
+        headers,
+      };
+    }
+  });
+
   return new ApolloClient({
-    request: async (operation) => {
-      try {
-        const accessToken = await authStorage.getAccessToken();
-        operation.setContext({
-          headers: {
-            authorization: accessToken ? `Bearer ${accessToken}` : '',
-          },
-        });
-      } catch (e) {
-        console.log(e);
-      }
-    },
-    uri: Constants.manifest.extra.apolloUri,
+    link: authLink.concat(httpLink),
+    cache,
   });
 };
 
